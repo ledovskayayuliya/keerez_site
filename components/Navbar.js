24bb0f41c22857707bff6bf34005c3919f14f7ae
ledovskayayuliya <65966183+ledovskayayuliya@users.noreply.github.com>
2020-09-11 15:35:52 +0300

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -15,13 +15,25 @@ export default class Navbar extends Component {
             <nav className='navbar'>
                 <div className='nav-center'>
                     <div className='nav-header'>
-                    <Link to='/'>
-                        <img src={logo} alt='Training services'/>
-                    </Link>
-                    <button type='button'>
-
-                    </button>
+                        <Link to='/'>
+                            <img src={logo} alt='Training services'/>
+                        </Link>
+                        <button 
+                            type='button'
+                            className='nav-btn'
+                            onClick={this.handleToggle}
+                        >
+                            <FaAlignRight className='nav-icon'/>
+                        </button>
                     </div>
+                    <ul className={this.state.isOpen?'nav-links show-nav':'nav-links'}>
+                     <li>
+                        <Link to='/'>Home</Link>
+                     </li>
+                     <li>
+                        <Link to='/services'>Services</Link>
+                     </li>
+                    </ul>
                 </div>
             </nav>
         )
