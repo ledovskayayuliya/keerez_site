2eac4c26ce67d6714448ead26e68a8b97cbf453f
ledovskayayuliya <65966183+ledovskayayuliya@users.noreply.github.com>
2020-09-11 14:15:13 +0300

    Delete SingleService.js

--- SingleService.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class SingleService extends Component {
-    render() {
-        return (
-            <div>Hello from single service page</div>
-        )
-    }
-}
