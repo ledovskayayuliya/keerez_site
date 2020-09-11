0ddf282f18df24cdec282b8d79b091e5b73c4eee
ledovskayayuliya <65966183+ledovskayayuliya@users.noreply.github.com>
2020-09-11 14:02:04 +0300

    Update App.js
    route, pages

--- App.js
+++ App.js
@@ -1,8 +1,27 @@
 import React from 'react';
-// import './App.css';
+//import './App.css';
+
+import Home from './pages/Home'
+import Services from './pages/Services'
+import SingleService from './pages/SingleService'
+import Error from './pages/Error'
+
+import {Route, Switch} from 'react-router-dom'
+
+import Navbar from './components/Navbar';
 
 function App() {
-  return <>hello from app</>;
+  return (
+  <>
+  <Navbar/>
+  <Switch>
+    <Route exact path='/' component={Home}/>
+    <Route exact path='/services/' component={Services}/>
+    <Route exact path='/services/:slug' component={SingleService}/>
+    <Route component={Error}/>
+    </Switch>
+  </>
+  );
 }
 
 export default App;
