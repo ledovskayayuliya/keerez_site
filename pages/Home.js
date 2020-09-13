b830af6c00cc8ac83744ec2186e9c531b05350e3
ledovskayayuliya <65966183+ledovskayayuliya@users.noreply.github.com>
2020-09-13 13:48:24 +0300

    Update Home.js
    add a banner

--- pages/Home.js
+++ pages/Home.js
@@ -1,5 +1,15 @@
 import React from 'react'
 import Hero from '../components/Hero';
+import Banner from '../components/Banner'
+import {Link} from 'react-router-dom'
 export default function Home() {
-    return <Hero hero='defaultHero'></Hero>;
+    return (
+    <Hero>
+        <Banner title='training services' subtitle='training advices, meal planes and other for everyone of you'>
+            <Link to='/services' className='btn-primary'>
+                our services
+            </Link>
+        </Banner>
+    </Hero>
+    );
 }
