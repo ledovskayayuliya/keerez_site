8ce951a5c803449d933f693e2cdc9e9f8823af88
ledovskayayuliya <65966183+ledovskayayuliya@users.noreply.github.com>
2020-09-14 21:53:19 +0300

    Update Home.js

--- pages/Home.js
+++ pages/Home.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
 import Hero from '../components/Hero';
-import Banner from '../components/Banner'
-import {Link} from 'react-router-dom'
+import Banner from '../components/Banner';
+import {Link} from 'react-router-dom';
+import Allservices from '../components/Allservices';
+import FeaturedServices from '../components/FeaturedServices';
 export default function Home() {
     return (
+    <>
     <Hero>
         <Banner title='training services' subtitle='training advices, meal planes and other for everyone of you'>
             <Link to='/services' className='btn-primary'>
@@ -11,5 +14,8 @@ export default function Home() {
             </Link>
         </Banner>
     </Hero>
+    <Allservices/>
+    <FeaturedServices/>
+    </>
     );
 }
